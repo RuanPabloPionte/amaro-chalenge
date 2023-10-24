ad2d45d96795567092cc6373dda45eaf62d94e95
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-24 13:40:32 -0300

    fix repositive of the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,19 +10,21 @@ import {
 function Header() {
   return (
     <header>
-      <nav className="flex flex-wrap justify-between shadow-md shadow-black p-4">
+      <nav className="flex flex-wrap justify-between shadow-md shadow-black p-3">
         {/* image */}
-        <div className="flex gap-2 ">
+        <div className="flex gap-2 order-1">
           <HiOutlineMenu className="md:hidden text-2xl" />
           <Image
             width={100}
             height={40}
             alt="amaro-logo"
             src="/Logotipo_da_Amaro.png"
+            className="md:p-2"
           />
         </div>
+
         {/* icons */}
-        <div className="flex gap-3 text-2xl">
+        <div className="flex gap-3 text-2xl md:text-3xl order-2 md:order-3">
           <span>
             <IoPersonOutline />
           </span>
@@ -30,13 +32,13 @@ function Header() {
             <IoBagOutline />
           </span>
         </div>
-        <div className="w-[100vw] text-lg">
+        <div className="md:order-2 order-last w-[100vw] md:w-[30vw] text-lg relative  my-3 md:m-0">
           <input
-            className="relative bg-slate-100 m-2 my-3 p-2 w-full border  border-black rounded-md"
+            className=" bg-slate-100  p-2 w-full border  border-black rounded-md"
             type="text"
             placeholder="O que você está procurando?"
           />
-          <IoSearchOutline className="absolute top-[8%] right-[20px] text-2xl" />
+          <IoSearchOutline className="absolute top-3 right-2 text-2xl" />
         </div>
       </nav>
     </header>
