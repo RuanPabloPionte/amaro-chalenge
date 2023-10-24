1bce13e4d0b14d785cf01bb5b341ac4a0a4928ca
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-24 19:45:44 -0300

    implemet itemCard component

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   // console.log(products[0]);
   return (
     <main>
-      <h1>Amaro</h1>
-      <section className="grid grid-cols-2">
+      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  m-5 gap-2 lg:gap-4">
         {prudcts.map((prudct, index) => (
           <ItemCard key={index} {...prudct} />
         ))}
--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -22,20 +22,47 @@ function ItemCard({
   sizes,
 }: Product) {
   //   console.log(products);
+  const hasDescount = discount_percentage && true;
   return (
-    <div
-      className="p-3"
-      style={{
-        border: "2px solid red",
-      }}
-    >
-      <Image src={image} width={200} height={400} alt={name} />
-      <p>{name}</p>
-      <p>{regular_price}</p>
-      <p>{actual_price}</p>
-      <p>{discount_percentage}</p>
-      {/* <p>{actual_price}</p> */}
-
+    <div className="p-3 gap-3 flex justify-center align-center flex-wrap bg-slate-200 rounded-md border border-black shadow-black shadow-md">
+      <Image
+        src={image}
+        width={300}
+        height={500}
+        alt={name}
+        className="w-full"
+      />
+      <div className=" text-lg lg:text-2xl font-semibold">
+        <p className="text-center">{name}</p>
+        <p>
+          {discount_percentage ? (
+            <>
+              <span className="line-through">{regular_price}</span>
+              <span className="text-emerald-600 mx-2">{`(${discount_percentage}) OFF`}</span>
+              <p>{actual_price}</p>
+            </>
+          ) : (
+            regular_price
+          )}
+        </p>
+      </div>
+      <div className="flex gap-2 w-full my-2 justify-center">
+        {sizes.map((size) =>
+          sizes.length > 3 ? (
+            <p
+              className={`border border-black p-2 rounded-md ${
+                size.available && "border-dashed"
+              }`}
+              key={size.sku}
+            >
+              {size.size}
+            </p>
+          ) : null
+        )}
+      </div>
+      <button className="border border-black rounded-md p-3 bg-slate-300 hover:bg-emerald-300 transition-all duration-[0.7s] shadow-md shadow-sky-400">
+        Add to Cart
+      </button>
     </div>
   );
 }
