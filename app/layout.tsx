c3a28e0bdd2f0fb8ac6f0d8503897cad1a9f83b1
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-31 11:02:28 -0300

    implement cartContext provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import ProductsContextProvider from "@/context/ProductsContext";
+import { CartContextProvider } from "@/context/CartContex";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ProductsContextProvider>
-          <Header />
-          {children}
+          <CartContextProvider>
+            <Header />
+            {children}
+          </CartContextProvider>
         </ProductsContextProvider>
       </body>
     </html>
