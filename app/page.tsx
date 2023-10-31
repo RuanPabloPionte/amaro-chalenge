1dc8ce0ad3a2701e13216993979ad6b760efe254
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-31 11:03:28 -0300

    changed the way i pass the props, now i'm passing the product itself

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main>
       <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  m-5 gap-2 lg:gap-4">
         {products.map((product, index) => (
-          <ItemCard key={index} {...product} />
+          <ItemCard key={index} product={product} />
         ))}
       </section>
     </main>
--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -11,64 +11,72 @@
 // - It must be possible to add products by size to the cart.
 // - It must be possible to view the cart with the items you've added (name, image, price, quantity) and the grand total.
 // - The cart should be persisted between reloads.
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Image from "next/image";
+import { useCart } from "@/context/CartContex";
+import ToastCart from "./ToastCart";
 
-function ItemCard({
-  id,
-  image,
-  name,
-  regular_price,
-  actual_price,
-  discount_percentage,
-  sizes,
-}: Product) {
-  //   console.log(products);
+type ItemCartProps = {
+  product: Product;
+};
+
+function ItemCard({ product }: ItemCartProps) {
+  const [openToast, setOpenToast] = useState(false);
   const [sizeCode, setSizeCode] = useState(" ");
-  console.log(id);
-
+  const { addProduct } = useCart();
   return (
     <div className="p-3 gap-3 flex justify-center align-center flex-wrap bg-slate-200 rounded-md border border-black shadow-black shadow-md">
       <Image
-        src={image}
+        src={product.image}
         width={300}
         height={500}
-        alt={name}
+        alt={product.name}
         className="w-full"
       />
       <div className=" text-lg lg:text-2xl font-semibold">
-        <p className="text-center">{name}</p>
+        <p className="text-center">{product.name}</p>
         <div>
-          {discount_percentage ? (
+          {product.discount_percentage ? (
             <>
-              <span className="line-through">{regular_price}</span>
-              <span className="text-emerald-600 mx-2">{`(${discount_percentage}) OFF`}</span>
-              <p>{actual_price}</p>
+              <span className="line-through">{product.regular_price}</span>
+              <span className="text-emerald-600 mx-2">{`(${product.discount_percentage}) OFF`}</span>
+              <p>{product.actual_price}</p>
             </>
           ) : (
-            regular_price
+            product.regular_price
           )}
         </div>
       </div>
       <div className="flex gap-2 w-full my-2 justify-center">
-        {sizes.map((size) =>
-          sizes.length > 3 ? (
+        {product.sizes.map((size) =>
+          product.sizes.length > 3 ? (
             <button
               disabled={size.available}
               className={`border border-black p-2 rounded-md ${
                 size.available && "border-dashed"
               }`}
               key={size.sku}
-              onClick={() => setSizeCode(size.sku)}
+              onClick={() => console.log(size.sku)}
             >
               {size.size}
             </button>
           ) : null
         )}
       </div>
-      <button className="border border-black rounded-md p-3 bg-slate-300 hover:bg-emerald-300 transition-all duration-[0.7s] shadow-md shadow-sky-400">
+      <button
+        onClick={() => {
+          addProduct(product);
+          setOpenToast(true);
+          setTimeout(() => {
+            setOpenToast(false);
+          }, 2 * 1000);
+        }}
+        className="border border-black rounded-md p-3 bg-slate-300 hover:bg-emerald-300 transition-all duration-[0.7s] shadow-md shadow-sky-400"
+      >
         Add to Cart
       </button>
+
+      {openToast && <ToastCart />}
     </div>
   );
 }
