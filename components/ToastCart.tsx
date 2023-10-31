35f8f18323bfaf2f133be42e673e8fa85012da9c
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-31 11:05:04 -0300

    implement Toast cart component

---
+++ components/ToastCart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToastCart() {
+  return (
+    <div className="bg-green-400 text-white p-3 rounded-md fixed right-0 bottom-0 z-10 m-2">
+      Produto adicionado ao carrinho
+    </div>
+  );
+}
+
+export default ToastCart;
