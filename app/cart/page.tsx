8d8591b32615682adb58029b5c3cfe5fef2e938a
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-31 14:38:45 -0300

    implement cart element

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContex";
+import Image from "next/image";
+import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
 
 type CartEntry = {
   product: Product;
@@ -8,7 +11,7 @@ type CartEntry = {
 };
 
 function Cart() {
-  const { cart } = useCart();
+  const { cart, deleteProduct } = useCart();
   const [cartList, setCartList] = useState([] as CartEntry[]);
   const [totalPrice, setTotalPrice] = useState(0);
   useEffect(() => {
@@ -46,10 +49,56 @@ function Cart() {
     getTotalPrice();
   }, [cart]);
   return (
-    <div>
+    // - It must be possible to view the cart with the items you've added (name, image, price, quantity) and the grand total.
+    <div className="bg-slate-200 min-h-screen ">
       {cartList.map((cart) => (
-        <div key={cart.product.id}>
-          <p>{cart.product.name}</p>
+        <div
+          className="relative flex m-2 p-2 gap-1 bg-white"
+          key={cart.product.id}
+        >
+          <Image
+            src={cart.product.image}
+            width={80}
+            height={100}
+            alt={cart.product.name}
+          />
+          <button
+            onClick={() => deleteProduct(cart.product.id)}
+            className="absolute top-5 right-5"
+          >
+            <AiOutlineClose />
+          </button>
+
+          <div className="flex flex-wrap m-2 w-[80%] gap-2">
+            <p className=" text-lg">{cart.product.name}</p>
+            <div className="w-full font-semibold ">
+              <p>
+                Cor:{" "}
+                <span className="text-black/50">
+                  {cart.product.color.toUpperCase()}
+                </span>{" "}
+              </p>
+              <p>
+                Tam:{" "}
+                <span className="text-black/50">
+                  {cart.product.sizes[0].size}
+                </span>{" "}
+              </p>
+            </div>
+            <div className="flex justify-between items-center gap-3 w-full mt-2">
+              <div className="flex gap-2 ">
+                <button className="text-2xl text-slate-500">
+                  <CiCircleMinus />
+                </button>
+                <p>{cart.quantity}</p>
+                <button 
+                className="text-2xl text-slate-500">
+                  <CiCirclePlus />
+                </button>
+              </div>
+              <p>{cart.product.actual_price}</p>
+            </div>
+          </div>
         </div>
       ))}
       <p>quantidade de produtos: {cart.length}</p>
